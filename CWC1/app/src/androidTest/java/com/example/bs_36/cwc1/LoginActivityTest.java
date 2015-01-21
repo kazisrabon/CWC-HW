34008b7dbcb35dec627487f78ed0ba1e62772107
unknown <kaziiit@gmail.com>
2015-01-20 23:09:15 -0800

    LoginActivityTest commited

--- CWC1/app/src/androidTest/java/com/example/bs_36/cwc1/LoginActivityTest.java
+++ CWC1/app/src/androidTest/java/com/example/bs_36/cwc1/LoginActivityTest.java
@@ -5,15 +5,17 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.robolectric.Robolectric;
 import org.robolectric.RobolectricTestRunner;
-import org.robolectric.annotation.Config;
-import org.fest.assertions.api.android.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import android.content.Intent;
+import android.widget.Button;
 /**
  * Created by BS-36 on 1/20/2015.
  */
 @RunWith(RobolectricTestRunner.class)
-@Config(emulateSdk = 18)
+
 public class LoginActivityTest {
-    private LoginActivityTest loginActivity;
+    private LoginActivity loginActivity;
 
     @Before
     public void setup(){
@@ -21,7 +23,16 @@ public class LoginActivityTest {
     }
 
     @Test
-    public void loginActivityAppearsAsExpectedInitially(){
-//        assert (loginActivity.btnLogin)
+    public void checkActivityNotNull() throws Exception {
+        assertNotNull(loginActivity);
+    }
+
+    @Test
+    public void buttonClickShouldStartNewActivity() throws Exception
+    {
+        Button button = (Button) loginActivity.findViewById(R.id.btnLogin);
+        button.performClick();
+        Intent intent = Robolectric.shadowOf(loginActivity).peekNextStartedActivity();
+        assertEquals(DashboardActivity.class.getCanonicalName(), intent.getComponent().getClassName());
     }
 }
